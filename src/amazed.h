f3ba26455f0dd89b73ed2f7af19f5ec7586392a6
xmarano <leo.gregori@epitech.eu>
2024-03-17 02:15:45 +0100

    ADD : Makefile & lib

---
+++ src/amazed.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-200-MAR-2-1-amazed-tom.clerc
+** File description:
+** amazed.h
+*/
+#include "my.h"
+#ifndef AMAZED_H
+    #define AMAZED_H
+
+typedef struct S {
+} S_t;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-200-MAR-2-1-amazed-tom.clerc
+** File description:
+** main.c
+*/
+#include "amazed.h"
+#include "my.h"
+
+int main(int argc, char **argv)
+{
+    S_t s;
+
+    my_printf("%s\n", argv[0]);
+}
